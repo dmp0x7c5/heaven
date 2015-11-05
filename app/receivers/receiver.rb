a33763481ea07df50abc9b12270a420869eaffe0
Michal Poczwardowski <michal.poczwardowski@netguru.pl>
2015-11-05 17:06:09 +0100

    Handle pull_request in Receiver

--- app/receivers/receiver.rb
+++ app/receivers/receiver.rb
@@ -53,8 +53,34 @@ class Receiver
       Resque.enqueue(Heaven::Jobs::DeploymentStatus, data)
     elsif event == "status"
       Resque.enqueue(Heaven::Jobs::Status, guid, data)
+    elsif event == "pull_request"
+      handle_pull_request
     else
       Rails.logger.info "Unhandled event type, #{event}."
     end
+  end
+
+  def handle_pull_request
+    # return unless data["action"].include? %w{synchronize open reopen}
+    # TODO: Check why data["action"] is overwritten by "create"
+    return if data["pull_request"]["state"] != "open"
+    deployment_payload_from_pull_request!
+    run_deployment!
+  end
+
+  def deployment_payload_from_pull_request!
+    data["deployment"] = {
+      "payload" => {
+        "config" => { "provider" => "capistrano_pull_request" },
+        "name" => "pull_request_number_#{data["number"]}"
+      },
+      "environment" => "staging",
+      "id" => data["pull_request"]["id"],
+      "sha" => data["pull_request"]["head"]["sha"],
+      "ref" => data["pull_request"]["head"]["ref"],
+      # Additional params for LockReceiver:
+      "creator" => { "login" => data["pull_request"]["user"]["login"] },
+      "task" => "deploy_pull_request"
+    }
   end
 end
